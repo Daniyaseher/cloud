277589da5d454b3fab14509726112461baf08a9e
Daniyaseher <63385253+Daniyaseher@users.noreply.github.com>
2020-05-12 15:37:20 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,14 @@
+package demo1;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int a=5;
+		 int b=10;
+		 int c;
+		 c= a*b;
+		 System.out.println(c);
+	}
+
+}
